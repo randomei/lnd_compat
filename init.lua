86a855e8d1ff8f743fa155326138783a5677445b
randomei <serg@randomei.com>
2024-12-13 11:20:14 +0500

    Offhand items pickup, fixed oil extract conflict, fixed farmin:string not in thread group

--- init.lua
+++ init.lua
@@ -1,16 +1,58 @@
-minetest.register_on_mods_loaded(function()
+local old_handle_node_drops = core.handle_node_drops
+
+function core.handle_node_drops(pos, drops, digger)
+	-- Add dropped items to object's inventory
+	local inv = digger and digger:get_inventory()
+	if not inv or not inv:get_list("offhand") then
+        old_handle_node_drops(pos, drops, digger)
+        return
+    end
+	local left_items = {}
+    local give_item = function(item)
+        return inv:add_item("offhand", item)
+    end
+    
+    for _, dropped_item in pairs(drops) do
+        if inv:contains_item("offhand", dropped_item) then
+            local left = give_item(dropped_item)
+            if not left:is_empty() then
+                table.insert(left_items, left)
+            end
+        else
+            table.insert(left_items, dropped_item)
+        end
+    end
+	old_handle_node_drops(pos, left_items, digger)
+
+end
+
+core.register_on_item_pickup(function(itemstack, picker, pointed_thing, time_from_last_punch,  ...)
+    local inv = picker and picker:get_inventory()
+    if not inv or not inv:get_list("offhand") then return end
+    if inv:contains_item("offhand", itemstack:get_name()) then
+        itemstack = inv:add_item("offhand", itemstack)
+    end
+	-- Pickup item.
+	if inv then
+		return inv:add_item("main", itemstack)
+	end
+    return itemstack
+end)
+
+core.register_on_mods_loaded(function()
     xcompat.materials.slab_stone = 'default:slab_stone_8'
     xcompat.materials.steel_bar = 'basic_materials:steel_bar'
     xcompat.materials.plastic_sheet = 'basic_materials:plastic_sheet'
-    minetest.register_alias("oil:oil", "techage:oil")
-    minetest.register_alias("oil:oil_source", "techage:oil_source")
-    minetest.register_alias("oil:oil_flowing", "techage:oil_flowing")
-    minetest.register_alias("oil:oil_bucket", "techage:bucket_oil")
-    minetest.register_alias("mesecraft_mobs:bone", "bonemeal:bone")
+    core.register_alias("oil:oil", "techage:oil")
+    core.register_alias("oil:oil_source", "techage:oil_source")
+    core.register_alias("oil:oil_flowing", "techage:oil_flowing")
+    core.register_alias("oil:oil_bucket", "techage:bucket_oil")
+    core.register_alias("mesecraft_mobs:bone", "bonemeal:bone")
 
-    minetest.clear_craft({output = "x_enchanting:grindstone"})
+    core.clear_craft({output = "x_enchanting:grindstone"})
+    core.clear_craft({output = "logistica:cobblegen_supplier"})
 
-    minetest.register_craft({
+    core.register_craft({
         output = 'x_enchanting:grindstone',
         recipe = {
             { 'group:stick', xcompat.materials.slab_stone, 'group:stick' },
@@ -18,12 +60,48 @@ minetest.register_on_mods_loaded(function()
         }
     })
 
-    minetest.register_craft({
+    core.register_craft({
         type = "cooking",
         output = "multidecor:wax_lump",
         recipe = "bees:wax",
         cooktime = 10
     })
+    
+    --fix ma and pops recipe conflict
+    local hedge_table = { --material, name
+        {'leaves', 'apple'},
+        {'pine_needles', 'pine'},
+        {'jungleleaves', 'jungle'},
+        {'acacia_leaves', 'acacia'},
+        {'aspen_leaves', 'aspen'}
+    }
+
+    for i in ipairs (hedge_table) do
+        local material = hedge_table[i][1]
+        local name = hedge_table[i][2]
+        
+        core.clear_craft({output = 'ma_pops_furniture:'..name..'_hedge'})
+
+        core.register_craft({
+            output = 'ma_pops_furniture:'..name..'_hedge',
+            recipe = {
+                {'default:'..material, 'default:'..material, ''},
+                {'default:'..material, 'default:'..material, ''},
+                {'default:'..material, 'default:'..material, ''}
+                }
+        })
+    end
+    --end fix ma and pops
+    
+    --fix farming:thread missing 'thread' group
+    local string_def = core.registered_items["farming:string"]
+    local new_groups = table.copy(string_def.groups or {})
+    new_groups.thread = 1
+
+    core.override_item("farming:string", {
+        groups = new_groups,
+    })
+    --fix farming:thread
     
     give_initial_stuff.items = {
         "default:axe_wood",
@@ -32,4 +110,4 @@ minetest.register_on_mods_loaded(function()
 end)
 
 
-minetest.log("info", "LnD-compat loaded successfully!")
+core.log("info", "LnD-compat loaded successfully!")
